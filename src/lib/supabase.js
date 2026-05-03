d129c3dfc09dd9c2a62ca11ecfd05d5c0ed370d7
Vinbracodetech <vinbracodetech@gmail.com>
2026-05-03 17:42:28 +0100

    Refactor Supabase client initialization

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://eartsscxtqxaelopmjmq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhcnRzc2N4dHF4YWVsb3Btam1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MTIwNjcsImV4cCI6MjA5MzM4ODA2N30.mqUbY0IaaUoNfJn3luTklD2_5NX8yOr8b1Z9aLEe7Bs'
-)
+const supabaseUrl = 'https://eartsscxtqxaelopmjmq.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhcnRzc2N4dHF4YWVsb3Btam1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MTIwNjcsImV4cCI6MjA5MzM4ODA2N30.mqUbY0IaaUoNfJn3luTklD2_5NX8yOr8b1Z9aLEe7Bs'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+})
