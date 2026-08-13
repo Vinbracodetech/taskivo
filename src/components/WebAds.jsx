22ed81ea30ac218e370cd3ff5f5b362284818665
Vinbracodetech <vinbracodetech@gmail.com>
2026-08-13 05:16:23 +0100

    Update SMART_LINKS with new Monetag links

--- src/components/WebAds.jsx
+++ src/components/WebAds.jsx
@@ -26,23 +26,20 @@ export function MonetagVignette() {
 }
 
 // ==========================================
-// 2. SMARTLINK ROTATOR ENGINE (Zero-Script Architecture)
+// 2. SMARTLINK ROTATOR ENGINE (10 Unique Monetag Links)
 // ==========================================
 
 export const SMART_LINKS = [
-  // Monetag
   'https://omg10.com/4/11559316',
   'https://omg10.com/4/11562945',
   'https://omg10.com/4/11565409',
   'https://omg10.com/4/11565422',
   'https://omg10.com/4/6424248',
   'https://omg10.com/4/11565430',
-  
-  // Adsterra
-  'https://www.effectivecpmnetwork.com/chezkq55y?key=76b312e871dafe4de49f97b2ad08fc06',
-  'https://www.effectivecpmnetwork.com/gppu1a3pke?key=a932494789184a47a4304656508aaacc',
-  'https://www.effectivecpmnetwork.com/tswcxv9a?key=8b23cafc193ceaf9521d2454138294a4',
-  'https://www.effectivecpmnetwork.com/h8a6z71n?key=69cfa93263741db3c0968045cd313e8a'
+  'https://omg10.com/4/11565745',
+  'https://omg10.com/4/11565747',
+  'https://omg10.com/4/11565748',
+  'https://omg10.com/4/11565749'
 ];
 
 export function getTaskSmartLink() {
