5747a25182d2c9bff561d46e1d6b098ab865c42d
Vinbracodetech <vinbracodetech@gmail.com>
2026-08-18 20:55:57 +0100

    Correct IS_MAINTENANCE_MODE assignment typo
    Fixed typo in maintenance mode variable assignment.

--- src/App.jsx
+++ src/App.jsx
@@ -122,7 +122,7 @@ function TopNav({ navigate, user, setAuthMode, theme, toggleTheme }) {
 export default function App() {
   // 🔴 MAINTENANCE KILL SWITCH 🔴
   // Set to 'true' to lock down the site. Set to 'false' to go live.
-  const IS_MAINTENANCE_MODE = false; 
+  const IS_MAINTENANCE_MODE = trye; 
 
   if (IS_MAINTENANCE_MODE) {
     return (
